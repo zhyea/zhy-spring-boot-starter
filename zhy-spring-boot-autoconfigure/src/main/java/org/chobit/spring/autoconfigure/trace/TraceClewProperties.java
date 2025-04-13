0f296a040986cb6e49a2f6a072e9a02a6353cfe3
zhyea <zhangrui@zhyea.com>
2025-04-13 22:49:11 +0800

    1. 修复启动时的warn提示
    Change-Id: Iaeef8fa1f7a92cefe34a483bdc9de76f76c30093

--- zhy-spring-boot-autoconfigure/src/main/java/org/chobit/spring/autoconfigure/trace/TraceClewProperties.java
+++ zhy-spring-boot-autoconfigure/src/main/java/org/chobit/spring/autoconfigure/trace/TraceClewProperties.java
@@ -1,18 +1,21 @@
 package org.chobit.spring.autoconfigure.trace;
 
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Role;
 
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 @ConfigurationProperties(prefix = "trace")
 public class TraceClewProperties {
 
-    private String flag;
+	private String flag;
 
 
-    public String getFlag() {
-        return flag;
-    }
+	public String getFlag() {
+		return flag;
+	}
 
-    public void setFlag(String flag) {
-        this.flag = flag;
-    }
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
 }
